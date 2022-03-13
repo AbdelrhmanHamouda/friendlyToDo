22407bc7c25df9454b90c00846411f106502a609
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2022-03-13 15:34:26 +0100

    Setup GET / CREATE / DELETE handling

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,16 +1,84 @@
+const { db } = require('../util/admin');
 
+// * GET todos
 exports.getAllTodos = (request, response) => {
-  todos = [
-    {
-      'id': '1',
-      'title': 'greeting',
-      'body': 'Hello world from Abdelrhman'
-    },
-    {
-      'id': '2',
-      'title': 'greeting2',
-      'body': 'Hello2 world2 from Marta'
+  db
+  .collection('todos')
+  // todo, use correct name and update database as well `createtAt >> createdAt`
+  .orderBy('createdAt', 'desc')
+  .get()
+  .then((data) => {
+    let todos = [];
+    data.forEach((doc) => {
+      todos.push({
+        todoId: doc.id,
+        title: doc.data().title,
+        body: doc.data().body,
+        createdAt: doc.data().createdAt,
+      });
+    });
+    return response.json(todos);
+  })
+  .catch((err) => {
+    console.error(err);
+    return response.status(500).json({ error: err.code});
+  });
+};
+
+
+// * POST todos
+exports.postOneTodo = (request, response) => {
+
+  // Validate incoming request
+  if (request.body.body.trim() === '') {
+    return response.status(400).json({ body: 'Must not be empty' });
+  }
+
+  if(request.body.title.trim() === '') {
+    return response.status(400).json({ title: 'Must not be empty' });
+  }
+
+  // Create newTodo object
+  const newTodoItem = {
+    title: request.body.title,
+    body: request.body.body,
+    createdAt: new Date().toISOString()
+  }
+
+  // Connect to db and:
+  // - Push to db
+  // - Construct response object
+  // - Catch error if any
+  db
+  .collection('todos')
+  .add(newTodoItem)
+  .then((doc)=>{
+    const responseTodoItem = newTodoItem;
+    responseTodoItem.id = doc.id;
+    return response.json(responseTodoItem);
+  })
+  .catch((err) => {
+    response.status(500).json({ error: 'Something went wrong' });
+    console.error(err);
+  });
+};
+
+
+// * Delete a ToDo
+exports.deleteTodo = (request, response) => {
+  const document = db.doc(`/todos/${request.params.todoId}`);
+  document.get()
+  .then((doc) => {
+    if (!doc.exists) {
+      return response.status(404).json({ error: 'Todo not found' })
     }
-  ]
-  return response.json(todos);
-}
+    return document.delete();
+  })
+  .then(() => {
+    response.json({ message: 'Deleted todo successfully' });
+  })
+  .catch((err) => {
+    console.error(err);
+    return response.status(500).json({ error: err.code });
+  });
+};
--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,19 @@ const app = require('express')();
 
 // Create a const pointing at our js file
 const {
-  getAllTodos
+  getAllTodos,
+  postOneTodo,
+  deleteTodo
 } = require('./APIs/todos')
 
 // Map incoming GET requests on /todos to `getAllTodos`
 app.get('/todos', getAllTodos);
+
+// Map incoming POST requests on /todos to `postOneTodo`
+app.post('/todos', postOneTodo);
+
+// Map incoming DELETE requests on /todos to `deleteTodo`
+app.delete('/todos/:todoId', deleteTodo);
 
 // Export a function `api`. When triggered, pass to `app`
 exports.api = functions.https.onRequest(app);
